b8708a3fb97329f7c36b84cdf529c7ea5b83e566
Adam Dąbrowski <contact@adamdabrowski.com>
2024-09-19 20:14:05 +0200

    patch: Implement “hello world”

---
+++ codex.scala
@@ -0,0 +1,10 @@
+//> using scala 3.5.1
+//> using jvm 22
+//> using packaging.packageType graalvm
+//> using packaging.graalvmArgs --no-fallback
+//> using packaging.output target/codex
+
+
+@main
+def run =
+    println("Hello, world!")
